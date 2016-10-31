bea73cf065a74782505367247141109a49285836
邓庆 <3035112@qq.com>
2016-10-31 22:01:34 +0800

    科目维护界面和Service

---
+++ Controllers/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Themes;
+using Orchard.Mvc;
+using Orchard;
+using Orchard.Localization;
+using System.Web.Mvc;
+
+namespace XinTuo.Accounts.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IOrchardServices _orchard;
+        
+
+        public AccountController(IOrchardServices orchard)
+        {
+            _orchard = orchard;
+        }
+
+
+        public ActionResult Account()
+        {
+            return new ShapeResult(this,_orchard.New.Account());
+        }
+
+        public Localizer T { get; set; }
+    }
+}
---
+++ Services/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XinTuo.Accounts.Models;
+using Orchard.Data;
+using Orchard.Security;
+using Orchard.ContentManagement;
+
+namespace XinTuo.Accounts.Services
+{
+    public class Account : IAccountcs
+    {
+        private readonly IContentManager _contentManager;
+        private readonly IRepository<AccountRecord> _accountRecord;
+
+        public Account(IContentManager contentManager,IRepository<AccountRecord> accountRecord)
+        {
+            _contentManager = contentManager;
+            _accountRecord = accountRecord;
+        }
+    }
+}
