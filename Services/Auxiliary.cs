c5c1eef585610a40d58c83c4612aa683cfb1c4f1
邓庆 <3035112@qq.com>
2016-11-17 11:46:42 +0800

    辅助核算功能

---
+++ Services/Auxiliary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Data;
+using Orchard.ContentManagement;
+using XinTuo.Accounts.Models;
+
+namespace XinTuo.Accounts.Services
+{
+    public class Auxiliary : IAuxiliary
+    {
+        private readonly IRepository<AuxiliaryTypeRecord> _auxType;
+        private readonly IContentManager _contentManager;
+
+        public Auxiliary(IRepository<AuxiliaryTypeRecord> auxType,IContentManager contentManager)
+        {
+            _auxType = auxType;
+            _contentManager = contentManager;
+        }
+
+        public List<AuxiliaryTypeRecord> GetBaseAuxType()
+        {
+            return _auxType.Table.ToList();
+        }
+
+        
+    }
+}
