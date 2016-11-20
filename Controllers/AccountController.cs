a657fb1986b6a91f4e07efa90505c8913f058d77
Administrator <Administrator@WIN-46287PIU2SL>
2016-11-20 19:21:03 +0800

    辅助核算及核算单位控制

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -16,20 +16,23 @@ namespace XinTuo.Accounts.Controllers
     {
         private readonly IOrchardServices _orchard;
         private readonly IAccountCategory _accCategory;
-        
+        private readonly IAuxiliary _auxiliary;
 
-        public AccountController(IOrchardServices orchard,IAccountCategory accCategory)
+
+        public AccountController(IOrchardServices orchard,IAccountCategory accCategory, IAuxiliary auxiliary)
         {
             _orchard = orchard;
             _accCategory = accCategory;
+            _auxiliary = auxiliary;
         }
 
 
         public ActionResult Account()
         {
             List<AccountCategoryRecord> cates = _accCategory.GetMainAccountCategory();
+            List<AuxiliaryTypeRecord> auxType = _auxiliary.GetBaseAuxType();
 
-            return new ShapeResult(this,_orchard.New.Account(Category : cates));
+            return new ShapeResult(this,_orchard.New.Account(Category : cates, AuxiliaryType: auxType));
         }
 
         public Localizer T { get; set; }
